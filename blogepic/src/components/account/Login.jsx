5200638871ab2414b699c2fc4992bef368ef6d23
jee-cmd <jeetishasharma@gmail.com>
2026-07-16 10:50:49 +0530

    Initial commit

--- blogepic/src/components/account/Login.jsx
+++
@@ -1,7 +0,0 @@
-import {Box, TextField} from "@mui/material";
-const Login = () => {
-    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
-    return (
-        <Box>
-            <img src={imageURL} alt="Logo" />   
-        </Box> className="login"> 
